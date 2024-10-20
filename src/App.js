5b1c0b48f552cb176990f10ac0db6d54d2bde21a
ThaiThiYenNhiBA <thaithiyennhiba@gmail.com>
2024-10-20 12:27:36 +0700

    config app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import DarkNav from './DarkNav';
+import Home from './Home';
+import Detail from './Detail';
+import CreateForm from './CreateForm';
+import EditForm from './EditForm';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Router>
+        <DarkNav />
+        <Switch>
+          <Route exact path="/blogs" component={Home} />
+          <Route exact path="/blogs/new" component={CreateForm} />
+          <Route exact path="/blogs/:id" component={Detail} />
+          <Route exact path="/blogs/:id/edit" component={EditForm} />
+          <Route path="/" component={Home} />
+          <Route component={Home} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
